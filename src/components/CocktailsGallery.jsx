0f85d4a9f36f36109c4678415a143ae47dda275e
yulen59 <j.delemarle@gmail.com>
2021-05-09 15:45:30 +0200

    finish useffect and props

--- src/components/CocktailsGallery.jsx
+++ src/components/CocktailsGallery.jsx
@@ -1,31 +1,36 @@
 import { useEffect, useState } from 'react';
 import axios from 'axios';
+import Cocktail from './Cocktail';
 
 function CocktailsGallery() {
   const [cocktails, setCocktails] = useState([]);
+  const [choiceAlcohol, setChoiceAlcohol] = useState('Non_Alcoholic');
+
+  const handleChangeAlcohol = (e) => {
+    setChoiceAlcohol(e.target.value);
+  };
 
   useEffect(() => {
     axios
       .get(
-        'https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Non_Alcoholic'
+        `https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=${choiceAlcohol}`
       )
       .then(({ data }) => {
         console.log(data.drinks);
         setCocktails(data.drinks);
       });
-  }, []);
+  }, [choiceAlcohol]);
 
   return (
     <>
       <h2>Cocktails</h2>
+      <select onChange={handleChangeAlcohol}>
+        <option value="Non_Alcoholic">sans alcool</option>
+        <option value="Alcoholic">avec alcool</option>
+      </select>
       <ul>
         {cocktails.map((cocktail) => {
-          return (
-            <li>
-              <h3>{cocktail.strDrink}</h3>
-              <img src={cocktail.strDrinkThumb} alt="" />
-            </li>
-          );
+          return <Cocktail {...cocktail} />;
         })}
       </ul>
     </>
